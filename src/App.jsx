28dfa5907b5dfb0c6875831cb08b0ad13d4f0b9f
avnms <avnmsarma@gmail.com>
2024-08-20 20:21:28 +1000

    Change title and favicon

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,11 @@
 import "./App.css";
 
 function App() {
-    return <h1>Portfolio WIP</h1>;
+    return (
+        <div>
+            <h1>Portfolio WIP</h1>
+        </div>
+    );
 }
 
 export default App;
