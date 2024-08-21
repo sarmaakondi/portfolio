6d742bfd4a38f15618a925e2ce442c568825742d
avnms <avnmsarma@gmail.com>
2024-08-21 11:48:25 +1000

    Fix issue with navigation, add redirect rule

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,7 +1,16 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useNavigate } from "react-router-dom";
 import "./Hero.css";
+import { useEffect } from "react";
 
 const Hero = () => {
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        if (window.location.pathname === "/portfolio") {
+            navigate("/portfolio/all");
+        }
+    }, [navigate]);
+
     return (
         <div className="hero-container">
             <Outlet />
