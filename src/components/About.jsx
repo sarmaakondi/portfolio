a504ade53040db47cb4eb59a4bf5a9f4164eeeb0
avnms <avnmsarma@gmail.com>
2024-08-21 22:33:54 +1000

    Implemented Social component and animate it

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,12 +1,24 @@
 import { motion } from "framer-motion";
 import AboutMe from "./AboutMe";
+import Social from "./Social";
 import "./About.css";
 
 const About = () => {
     return (
         <div className="about-container">
-            <motion.div className="work-item">
+            <motion.div
+                className="work-item"
+                initial={{ opacity: 0, x: 500, y: 250 }}
+                animate={{ opacity: 1, x: 0, y: 0 }}
+                transition={{ duration: 0.8, ease: "easeOut" }}>
                 <AboutMe />
+            </motion.div>
+            <motion.div
+                className="work-item"
+                initial={{ opacity: 0, x: -500, y: -50 }}
+                animate={{ opacity: 1, x: 0, y: 0 }}
+                transition={{ duration: 0.8, ease: "easeOut" }}>
+                <Social />
             </motion.div>
         </div>
     );
---
+++ src/components/Social.jsx
@@ -0,0 +1,76 @@
+import { useState } from "react";
+import UseAnimations from "react-useanimations";
+import github from "react-useanimations/lib/github";
+import linkedin from "react-useanimations/lib/linkedin";
+import mail from "react-useanimations/lib/mail";
+import "./Social.css";
+
+const Social = () => {
+    const [isGithubHovered, setIsGithubHovered] = useState(false);
+    const [isLinkedInHovered, setIsLinkedInHovered] = useState(false);
+    const [isMailHovered, setIsMailHovered] = useState(false);
+
+    const handleGithubHoverEnter = () => {
+        setIsGithubHovered(true);
+    };
+
+    const handleGithubHoverLeave = () => {
+        setIsGithubHovered(false);
+    };
+
+    const handleLinkedInHoverEnter = () => {
+        setIsLinkedInHovered(true);
+    };
+
+    const handleLinkedInHoverLeave = () => {
+        setIsLinkedInHovered(false);
+    };
+
+    const handleMailHoverEnter = () => {
+        setIsMailHovered(true);
+    };
+
+    const handleMailHoverLeave = () => {
+        setIsMailHovered(false);
+    };
+
+    return (
+        <div className="social-container">
+            <h2>Get in touch</h2>
+            <div className="social-links-container">
+                <div
+                    onMouseEnter={handleGithubHoverEnter}
+                    onMouseLeave={handleGithubHoverLeave}>
+                    <UseAnimations
+                        key={isGithubHovered === true}
+                        animation={github}
+                        size={100}
+                        autoplay={isGithubHovered}
+                    />
+                </div>
+                <div
+                    onMouseEnter={handleLinkedInHoverEnter}
+                    onMouseLeave={handleLinkedInHoverLeave}>
+                    <UseAnimations
+                        key={isLinkedInHovered === true}
+                        animation={linkedin}
+                        size={100}
+                        autoplay={isLinkedInHovered}
+                    />
+                </div>
+                <div
+                    onMouseEnter={handleMailHoverEnter}
+                    onMouseLeave={handleMailHoverLeave}>
+                    <UseAnimations
+                        key={isMailHovered === true}
+                        animation={mail}
+                        size={100}
+                        autoplay={isMailHovered}
+                    />
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Social;
