d9a08d587fab1d266d4ac4a1b9dabab6999b860b
avnms <avnmsarma@gmail.com>
2024-08-21 20:35:03 +1000

    Add Explore icon and animation

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -1,10 +1,37 @@
+import UseAnimations from "react-useanimations";
+import explore from "react-useanimations/lib/explore";
 import "./Project.css";
+import { useState } from "react";
 
 const Job = ({ project }) => {
+    const [isHovered, setIsHovered] = useState(false);
+
+    const handleMouseEnter = () => {
+        setIsHovered(true);
+    };
+
+    const handleMouseLeave = () => {
+        setIsHovered(false);
+    };
+
     return (
         <div className="project-container">
             <img className="project-image" src={project.image} alt="" />
-            <h2 className="project-name">{project.name}</h2>
+            <div className="project-name-container">
+                <h2 className="project-name">{project.name}</h2>
+                <div
+                    className="project-explore"
+                    onMouseEnter={handleMouseEnter}
+                    onMouseLeave={handleMouseLeave}>
+                    <UseAnimations
+                        key={isHovered === true}
+                        animation={explore}
+                        size={40}
+                        strokeColor="gray"
+                        autoplay={isHovered}
+                    />
+                </div>
+            </div>
             <ul className="technology-container">
                 {project.tech.map((tech, index) => (
                     <li key={index} className="technology-name">
