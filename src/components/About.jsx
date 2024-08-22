2dcd6d92acfa1f1759937bea49b042fa254ebc4d
avnms <avnmsarma@gmail.com>
2024-08-22 12:23:24 +1000

    Add bubble animation

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,24 +1,32 @@
 import { motion } from "framer-motion";
 import AboutMe from "./AboutMe";
 import Social from "./Social";
+import Skills from "./Skills";
 import "./About.css";
 
 const About = () => {
     return (
         <div className="about-container">
             <motion.div
-                className="work-item"
+                className="about-item"
                 initial={{ opacity: 0, x: 500, y: 250 }}
                 animate={{ opacity: 1, x: 0, y: 0 }}
                 transition={{ duration: 0.8, ease: "easeOut" }}>
                 <AboutMe />
             </motion.div>
             <motion.div
-                className="work-item"
+                className="about-item"
                 initial={{ opacity: 0, x: -500, y: -50 }}
                 animate={{ opacity: 1, x: 0, y: 0 }}
                 transition={{ duration: 0.8, ease: "easeOut" }}>
                 <Social />
+            </motion.div>
+            <motion.div
+                className="about-item-full"
+                initial={{ opacity: 0, y: 450 }}
+                animate={{ opacity: 1, y: 0 }}
+                transition={{ duration: 0.8, ease: "easeOut" }}>
+                <Skills />
             </motion.div>
         </div>
     );
---
+++ src/components/Skills.jsx
@@ -0,0 +1,14 @@
+import "./Skills.css";
+
+const Skills = () => {
+    return (
+        <div className="skills-container">
+            <h2>
+                Tech I <span className="skills-emoji">&#128525;</span> and work
+                with
+            </h2>
+        </div>
+    );
+};
+
+export default Skills;
