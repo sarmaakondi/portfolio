ad64a23f8e646944f3eca6c4c97f0bb1bab57199
avnms <avnmsarma@gmail.com>
2024-08-26 08:01:08 +1000

    Enable text compression

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,15 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import compression from "vite-plugin-compression";
 
 export default defineConfig({
-    plugins: [react()],
+    plugins: [
+        react(),
+        compression({
+            algorithm: "gzip",
+            ext: ".gz",
+            threshold: 1024,
+        }),
+    ],
     base: "/portfolio/",
 });
