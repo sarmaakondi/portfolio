0a17eff17c971dd09ebbd22108538b167dbf3a92
avnms <avnmsarma@gmail.com>
2024-08-26 11:21:31 +1000

    Refactor to ensure List contains only li items

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -9,53 +9,30 @@ const Skills = () => {
                 Tech I <img className="skills-emoji" src={HeartImage} alt="" />{" "}
                 and work with
             </h2>
+
             <ul className="skills-list">
-                <div className="skills-icon-container">
-                    <li>
+                <li>
+                    <div className="skills-icon-container">
                         <StackIcon name="html5" />
-                    </li>
-                    <li>
                         <StackIcon name="css3" />
-                    </li>
-                    <li>
                         <StackIcon name="tailwindcss" />
-                    </li>
-                    <li>
                         <StackIcon name="bootstrap5" />
-                    </li>
-                    <li>
                         <StackIcon name="js" />
-                    </li>
-                    <li>
                         <StackIcon name="reactjs" />
-                    </li>
-                    <li>
                         <StackIcon name="nodejs" />
-                    </li>
-                </div>
-                <div className="skills-icon-container">
-                    <li>
+                    </div>
+                </li>
+                <li>
+                    <div className="skills-icon-container">
                         <StackIcon name="python" />
-                    </li>
-                    <li>
                         <StackIcon name="django" />
-                    </li>
-                    <li>
                         <StackIcon name="flask" />
-                    </li>
-                    <li>
                         <StackIcon name="mongodb" />
-                    </li>
-                    <li>
                         <StackIcon name="mongoose" />
-                    </li>
-                    <li>
                         <StackIcon name="postgresql" />
-                    </li>
-                    <li>
                         <StackIcon name="mysql" />
-                    </li>
-                </div>
+                    </div>
+                </li>
             </ul>
         </div>
     );
